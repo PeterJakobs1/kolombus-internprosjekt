ca30100dd7ec54c6342ef2f8f1d33a9f407c8091
PeterJakobs1 <peter.bj@live.no>
2023-09-15 10:58:24 +0200

    gh actions playwright test

--- Frontend/tests/tests.spec.test.js
+++ Frontend/tests/tests.spec.test.js
@@ -1,52 +1,36 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto('https://kolombus-reskin.azurewebsites.net');
   await expect(page).toHaveTitle(/Kolombus-reskin/);
 });
 
-
 test('has button hover', async ({ page }) => {
-
-  await page.goto('http://localhost:5173/');
+  await page.goto('https://kolombus-reskin.azurewebsites.net');
 
   const buttonSelector = 'button.Innstillinger';
 
   await page.waitForSelector(buttonSelector);
   await page.hover(buttonSelector);
 
-
-  const buttonBackgroundColor = await page.$eval(
-    buttonSelector,
-    (button) => getComputedStyle(button).getPropertyValue('background-color')
+  const buttonBackgroundColor = await page.$eval(buttonSelector, (button) =>
+    getComputedStyle(button).getPropertyValue('background-color')
   );
 
-  expect(buttonBackgroundColor).toBe('rgb(249, 249, 249)'); 
+  expect(buttonBackgroundColor).toBe('rgb(249, 249, 249)');
 });
 
-test('dropdown has placeholder', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+// test('dropdown has placeholder', async ({ page }) => {
+//   await page.goto('https://kolombus-reskin.azurewebsites.net');
 
+//   const selectedOption = 'p.searchDropDown';
 
-  const selectedOption = 'p.searchDropDown'; 
+//   await page.waitForSelector(selectedOption);
 
-  await page.waitForSelector(selectedOption);
+//   const dropdownText = await page.$eval(
+//     selectedOption,
+//     (dropdown) => dropdown.textContent
+//   );
 
-  const dropdownText = await page.$eval(
-    selectedOption,
-    (dropdown) => dropdown.textContent
-  );
-
-  expect(dropdownText.trim()).toBe('Søk...');
-});
-
-
-
-
-
-
-
-
-
-
-
+//   expect(dropdownText.trim()).toBe('Søk...');
+// });
